01617448ea6dc0f83adc3310650ea22da4ff00aa
Davide <davide.magnoni@luxottica.com>
2023-03-07 17:10:38 +0100

    add locomotive

--- pages/SGH/SGH_Example/js/index.js
+++ pages/SGH/SGH_Example/js/index.js
@@ -1,4 +1,20 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
 document.addEventListener('DOMContentLoaded',()=>{
-    window.promo = true;
+    window.promo = false;
+    const lsscroll = new LocomotiveScroll({
+        el: document.querySelector('#content'),
+        smooth: true,
+        lerp: 0.5,
+        reloadOnContextChange: true,
+        tablet: {
+            smooth: true,
+        },
+        smartphone: {
+            smooth: false,
+        }
+    });
     
+    
+      
 })
