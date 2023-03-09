87d59135300467df0ab9ddd1b8b0ea3a00a1890a
Davide <davide.magnoni@luxottica.com>
2023-03-09 17:19:04 +0100

    some fixes common file SGH

--- pages/SGH/SGH_LensHubHome/js/index.js
+++ pages/SGH/SGH_LensHubHome/js/index.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded',()=>{
             centerMode: true,
             infinite: true,
             easing: 'ease',
+            //  autoplay: true,
             speed: 600,
             prevArrow: '<button type="button" class="slick-prev"><svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 22C0 9.84974 9.84974 0 22 0C34.1503 0 44 9.84974 44 22C44 34.1503 34.1503 44 22 44C9.84974 44 0 34.1503 0 22Z" fill="#222222"/><path d="M19 30L27 22L19 14" stroke="white" stroke-width="3" stroke-linejoin="round"/></svg</button>',
             nextArrow: '<button type="button" class="slick-next"><svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 22C0 9.84974 9.84974 0 22 0C34.1503 0 44 9.84974 44 22C44 34.1503 34.1503 44 22 44C9.84974 44 0 34.1503 0 22Z" fill="#222222"/><path d="M19 30L27 22L19 14" stroke="white" stroke-width="3" stroke-linejoin="round"/></svg</button>',
