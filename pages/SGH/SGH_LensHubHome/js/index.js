e8850cf913fe8e6d00111c5157c9a8258889c768
Davide <davide.magnoni@luxottica.com>
2023-03-20 20:55:04 +0100

    fix slider animation and slider style

--- pages/SGH/SGH_LensHubHome/js/index.js
+++ pages/SGH/SGH_LensHubHome/js/index.js
@@ -7,23 +7,36 @@ function currentDevice() {
     return window.innerWidth > 1025 ? "D" : window.innerWidth < 1025 && window.innerWidth >= 768 ? "T" :  window.innerWidth < 768 ? "M" : "not recognized";
 }
 
+function entryHero(){
 
+}
+
+function  entryAnim(){
+    for (const [key, value] of Object.entries(navManger.sectionsTopOffest)) {
+        if(window.scrollY + Math.floor(window.innerHeight/100*80) - value.top > 0 && !document.querySelector(`[data-section="${key}"]`).classList.contains('ct_in')){
+            document.querySelector(`[data-section="${key}"]`).classList.add('ct_in')
+        }
+      
+    }
+}
 
 
 
 document.addEventListener('DOMContentLoaded',()=>{
     window.ct_current__device = currentDevice();
     window.promo = false;
+    entryHero();
     lazyLo();
     videoManager.init();
-   
     navManger.init();
+    entryAnim()
 
 })
 
 document.addEventListener('scroll',()=>{
-  
+    
     lazyLo();
+    entryAnim();
 })
 
 
@@ -38,12 +51,13 @@ $(document).ready(function(){
         asNavFor: '.slider-images',
         speed: 600,
         dots:true,
+        rows:0,
         prevArrow: '<button type="button" class="slick-prev"><svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.64672 11.8535L2.79297 5.99998L8.64672 0.146484L9.35372 0.853484L4.20747 5.99998L9.35372 11.1465L8.64672 11.8535Z" fill="#555555"/> </svg>  </button>',
         nextArrow: '<button type="button" class="slick-next"><svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.35348 11.8535L3.64648 11.1465L8.79273 5.99998L3.64648 0.853484L4.35348 0.146484L10.2072 5.99998L4.35348 11.8535Z" fill="#555555"/></svg> </button>',
       
     });
     $('.slider-images').slick({
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.slider-text',
         dots: false,
@@ -53,23 +67,39 @@ $(document).ready(function(){
         easing: 'ease',
         autoplay: true,
         speed: 600,
-      
+        centerPadding: '28%',
+        rows:0,
+        
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 1024,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                     slidesToScroll: 1,
                     centerMode: true,
                     arrows: false,
                     draggable: true,
-                    centerPadding: '0px'
+                    centerPadding: '22%',
                 }
             }
             
         ]
     });
-    $('.slider-text .slick-dots').append(`<span> / ${ $('.slider-text .slick-dots')[0]?.childElementCount}</span>`)
+
+    // Add custom class to next or previous slide depending on change direction
+    $('.slider-images').on('beforeChange', (event, slick, currentSlide, nextSlide) => {
+        let nextIndex = currentSlide + 1; // assume moving right
+        if(currentSlide-1 == nextSlide || (nextSlide+1 == slick.slideCount && currentSlide < nextSlide)) {
+            nextIndex = currentSlide - 1; // nope, moving left
+        }
+        $(`[data-slick-index="${nextIndex}"]`).addClass('slick-target');
+    });
+
+    // clear custom class after transition
+    $('.slider-images').on('afterChange', () => {
+        $('.slick-slide').removeClass('slick-target');
+    });
+    $('.slider-text .slick-dots').append(`<li class="ct_slide_count"><button> / ${ $('.slider-text .slick-dots')[0]?.childElementCount}</button></li>`)
 
 });
 // $('#slider').slick({
