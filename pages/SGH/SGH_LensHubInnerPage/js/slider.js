1c6b0407c6ce76b78b83522bcd83125c7d863e74
Alessia Bottinelli <alessia.bottinelli@luxottica.com>
2023-03-29 17:25:51 +0200

    slick play-pause function

--- pages/SGH/SGH_LensHubInnerPage/js/slider.js
+++ pages/SGH/SGH_LensHubInnerPage/js/slider.js
@@ -20,6 +20,20 @@ class Slider {
         accessibility: false,
         cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
     });
+    this.set_slider_control();
+  }
+  set_slider_control(){
+    let controls = document.querySelector('.ct_section__best-usage .ct_wrapper__carousel .ct_video__controls');
+    controls.addEventListener('click', (e)=>{
+      if(controls.classList.contains("ct__playing")){
+        $('.ct_carousel').slick('slickPause');
+        controls.classList.remove('ct__playing');
+      }
+      else{
+        $('.ct_carousel').slick('slickPlay');
+        controls.classList.add('ct__playing')
+      }
+    });
   }
 }
 
