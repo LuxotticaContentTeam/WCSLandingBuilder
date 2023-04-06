5c20a005a804a0eaaca07e8051b5dc9d3ad8e2ea
Davide <davide.magnoni@luxottica.com>
2023-04-06 11:54:40 +0200

    saving mode done

--- pages/CD/CD_LowPowerMode/js/index.js
+++ pages/CD/CD_LowPowerMode/js/index.js
@@ -1,1 +1,41 @@
 // console.log('%c WCS LANDING BUILDER IT\'S WORKING','background:green;color:white;border-radius:8px;padding:4px;')
+
+function monetateCode(){
+    console.log('MONETATE');
+    console.log('%cRISPARMIO ENERGETICO CHECK','background:blue;color:#fff;');
+    let video = document.querySelector('[data-czid="X_HP_Hero"] video');
+    if (video.paused  === false){
+        console.log('%cRISPARMIO ENERGETICO NON ATTIVO','background:green;color:#fff');
+    }else{
+        if (video.readyState > 1){
+            video.play()
+            .then(function(){
+              console.log('%cRISPARMIO ENERGETICO NON ATTIVO','background:green;color:#fff');
+          })
+          .catch(function(error){
+               console.log('%cRISPARMIO ENERGETICO ATTIVO','background:yellow');
+          });
+        }else{
+            video.addEventListener('canplay',()=>{
+                video.play()
+                .then(function(){
+                  console.log('%cRISPARMIO ENERGETICO NON ATTIVO','background:green;color:#fff');
+              })
+              .catch(function(error){
+                   console.log('%cRISPARMIO ENERGETICO ATTIVO','background:yellow');
+              });
+            })
+        }
+       
+    }
+    
+   
+    
+    
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    setTimeout(()=>{
+        monetateCode()
+    },15000)
+})
--- utils/dependences/CD/index.js
+++ utils/dependences/CD/index.js
@@ -1,6 +1,22 @@
-
-
-document.addEventListener('DOMContentLoaded',()=>{
+// function ct_cos__load_script(url) {
+    
+  
+  
+//       var script = document.createElement("script");
+      
+      
+//       script.src = url  
+//       document.body.appendChild(script);
    
+// }
+
+// function randomTime(min,max){
+//     return   Math.floor( Math.random() * (max - min) + min);
+// }
+
+// document.addEventListener('DOMContentLoaded',()=>{
+//     setTimeout(()=>{
+//         ct_cos__load_script("../../../../../utils/pa/CD/index.js")
+//     },randomTime(1000,3000))
    
-})
+// })
